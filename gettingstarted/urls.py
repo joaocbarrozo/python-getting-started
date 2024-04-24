c4176add513e34c6ade74953a25bb06be0ff277b
Joao Carlos <joaocbarrozo@gmail.com>
2024-04-24 09:14:55 -0300

    Liberação do admin

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 # from django.contrib import admin
 from django.urls import path, include
+from django.contrib import admin
 
-import hello.urls
+#import hello.urls
 import hello.views
 
 urlpatterns = [
@@ -25,5 +26,5 @@ urlpatterns = [
     #path("db/", hello.views.db, name="db"),
     # Uncomment this and the entry in `INSTALLED_APPS` if you wish to use the Django admin feature:
     # https://docs.djangoproject.com/en/5.0/ref/contrib/admin/
-    # path("admin/", admin.site.urls),
+    path("admin/", admin.site.urls),
 ]
